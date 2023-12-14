06845bfddfb8a55bc60b0dc3f32b2a1cc5913351
Benja203 <benjamin.alvarado.1510@gmail>
2023-12-14 00:52:36 -0500

    trabajo para casa terminado

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -16,6 +16,38 @@ class PagesController extends Controller
         return view('Estudiante.pagDetalle', compact('xDetAlumnos'));
     }
 
+    public function fnEstActualizar($id){
+        $xActAlumnos = Estudiante::findOrFail($id);
+        return view('Estudiante.pagActualizar', compact('xActAlumnos'));
+    }
+
+    public function fnUpdate(Request $request, $id){
+        
+        $xUpdateAlumnos = Estudiante::findOrFail($id);
+
+        $xUpdateAlumnos -> codEst = $request -> codEst;
+        $xUpdateAlumnos -> nomEst = $request -> nomEst;
+        $xUpdateAlumnos -> apeEst = $request -> apeEst;
+        $xUpdateAlumnos -> fnaEst = $request -> fnaEst;
+        $xUpdateAlumnos -> turMat = $request -> turMat;
+        $xUpdateAlumnos -> semMat = $request -> semMat;
+        $xUpdateAlumnos -> estMat = $request -> estMat;
+
+        $xUpdateAlumnos -> save();
+
+        //$xAlumnos = Estudiante::all();                    Datos BD
+        //return view('pagLista', compact('xAlumnos));        Equivalente
+
+        return back()->with('msj','Se actualizó con éxito...');
+    }
+
+    public function fnEliminar($id){
+        $deleteAlumno = Estudiante::findOrFail($id);
+        $deleteAlumno->delete();
+        return back()->with('msj', 'Se elimino con éxito...');
+    }
+    
+    
     public function fnLista () {
         $xAlumnos = Estudiante::all();   //Datos de BD
         return view('pagLista', compact('xAlumnos'));
@@ -29,4 +61,35 @@ class PagesController extends Controller
         //return view('pagGaleria', ['valor'=>$, 'otro' =>25]);
         return view('pagGaleria', compact('valor', 'otro'));
     }
+
+    public function fnRegistrar(Request $request){
+
+        //return $request->all();         //Prueba de "token" y datos recibidos
+
+        $request ->validate([
+            'codEst' => 'required',
+            'nomEst' => 'required',
+            'apeEst' => 'required',
+            'fnaEst' => 'required',
+            'turMat' => 'required',
+            'semMat' => 'required',
+            'estMat' => 'required'
+        ]);
+
+        $nuevoEstudiante = new Estudiante;
+        $nuevoEstudiante->codEst = $request->codEst;
+        $nuevoEstudiante->nomEst = $request->nomEst;
+        $nuevoEstudiante->apeEst = $request->apeEst;
+        $nuevoEstudiante->fnaEst = $request->fnaEst;
+        $nuevoEstudiante->turMat = $request->turMat;
+        $nuevoEstudiante->semMat = $request->semMat;
+        $nuevoEstudiante->estMat = $request->estMat;
+        
+        $nuevoEstudiante->save();
+        
+        //$xAlumnos = Estudiante1::all();                      //Datos de BD
+        //return view('pagLista', compact('xAlumnos'));        //Pasar a pagLista
+        return back()->with('msj','Se registro con éxito...'); //Regresar con msj
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,15 @@ use App\Http\Controllers\PagesController;
 
 Route::get('/', [PagesController::class, 'fnIndex']) -> name('xInicio');
 
+Route::post('/', [PagesController::class, 'fnRegistrar']) -> name('Estudiante.xRegistrar');
+
 Route::get('/detalle/{id}', [PagesController::class, 'fnEstDetalle']) -> name('Estudiante.xDetalle');
+
+Route::get('/actualizar/{id}', [PagesController::class, 'fnEstActualizar']) -> name('Estudiante.xActualizar');
+
+Route::put('/actualizar/{id}', [PagesController::class, 'fnUpdate']) -> name('Estudiante.xUpdate');
+
+Route::delete('/eliminar/{id}', [PagesController::class, 'fnEliminar']) -> name('Estudiante.xEliminar');
 
 Route::get('/galeria/{numero?}', [PagesController::class, 'fnGaleria']) -> where('numero', '[0-9]+') -> name('xGaleria');
 
